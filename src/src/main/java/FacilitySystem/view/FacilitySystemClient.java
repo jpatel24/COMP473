7fb3ca397e50bb1224b2b86a8fcdcaf7b8e058d3
ethanle993 <ele@luc.edu>
2015-03-16 00:55:35 -0500

    added Spring configurations

--- src/src/main/java/FacilitySystem/view/FacilitySystemClient.java
+++ src/src/main/java/FacilitySystem/view/FacilitySystemClient.java
@@ -1,8 +1,13 @@
 package src.main.java.FacilitySystem.view;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
 import src.main.java.FacilitySystem.*;
 import src.main.java.FacilitySystem.service.FacilityService;
 import src.main.java.FacilitySystem.service.MaintenanceService;
+import sun.applet.Main;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -20,42 +25,59 @@ public class FacilitySystemClient {
 
     public static void main(String[]args) throws Exception{
 
-        FacilityService facilityService = new FacilityService();
-        MaintenanceService maintenanceService = new MaintenanceService();
-        Person person = new Person();
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        //FacilityService facilityService = new FacilityService();
+        FacilityService facilityService = (FacilityService) context.getBean("facilityService");
+        //MaintenanceService maintenanceService = new MaintenanceService();
+        MaintenanceService maintenanceService = (MaintenanceService) context.getBean("maintenanceService");
+
+
+        //Person person = new Person();
+        Person person = (Person) context.getBean("person");
         person.setAge(23);
         person.setFirstName("Jeel");
         person.setLastName("Patel");
 
-        Facility facility1 = new Facility();
+
+        //Facility facility1 = new Facility();
+        Facility facility1 = (Facility) context.getBean("facility");
         facility1.setCost(700);
         facility1.setDescription("Admissons and Registras  ");
         facility1.setVacant(false);
-        facility1.setOwner(person);
+        //facility1.setOwner(person);
        // facilityService.addFacility(facility1);
 
 
         Calendar calendar = new GregorianCalendar();
-        Use use = new Use();
-        use.setFacility(facility1);
-        use.setUseDescription("Filing records");
-       // use.setStartDate(calendar.set(2004,5,13));
-        use.setId(2);
 
-        List<Maintenance> maintenanceList = new ArrayList<Maintenance>();
-        MaintenanceRequest maintenanceRequest = new MaintenanceRequest();
+
+        //Use use = new Use();
+        Use use = (Use) context.getBean("use");
+        //use.setFacility(facility1);
+        use.setUseDescription("Filing records");
+        use.setId(2);
+      //use.setStartDate(calendar.set(2004,5,13));
+
+
+
+        //List<Maintenance> maintenanceList = new ArrayList<Maintenance>();
+        List<Maintenance> maintenanceList = (ArrayList) context.getBean("maintenanceList");
+        //MaintenanceRequest maintenanceRequest = new MaintenanceRequest();
+        MaintenanceRequest maintenanceRequest = (MaintenanceRequest) context.getBean("maintenanceRequest");
         maintenanceRequest.setFacility(facility1);
         maintenanceRequest.setCurrent(true);
         maintenanceRequest.setEstCost(50);
         maintenanceRequest.setMaintenances(maintenanceList);
 
-        Maintenance maintenance = new Maintenance();
+        //Maintenance maintenance = new Maintenance();
+        Maintenance maintenance = (Maintenance) context.getBean("maintenance");
         maintenance.setDescription("Leaky faucet");
         maintenance.setCost(300);
         maintenance.setMaintenanceRequest(maintenanceRequest);
         //maintenanceService.addMaintenance(maintenance,maintenanceRequest);
 
-        FacilityProblems facilityProblems = new FacilityProblems();
+        //FacilityProblems facilityProblems = new FacilityProblems();
+        FacilityProblems facilityProblems = (FacilityProblems) context.getBean("facilityProblems");
         facilityProblems.setDescription("heater broken");
         facilityProblems.setCurrent(false);
         facilityProblems.setFacility(facility1);
